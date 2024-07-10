ec1b612c1ad679ba1a75f760e4b75e6cd6ab681b
Rahul Tiwari <tiwari.rahul1403@gmail.com>
2024-07-10 16:08:51 +0530

    deletion of unwanted boiler plate
    this boiler plate was added by mistake which was restricting the code to run, so i have removed it

--- index.php
+++ index.php
@@ -3,14 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feedback Form</title>
     <link rel="stylesheet" href="style.css">
     
@@ -52,5 +44,3 @@
 </body>
 </html>
 
-</body>
-</html>
